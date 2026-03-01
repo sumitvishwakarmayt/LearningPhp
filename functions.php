1efabde63219268a3bd79efd2911ce1c7e1a8963
Sumit Vishwakarma <sumitvishwakarma.tech@gmail.com>
2026-03-01 10:18:42 +0530

    Exploring Functions

--- functions.php
+++ functions.php
@@ -3,16 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Functions in Php</title>
 </head>
 <body>
-    <h1>Will Explore functions in Php @27th Feb 2026.</h1>
+    <h1>Creating and Exploring Functions in Php</h1>
 </body>
 </html>
 <?php
+
+/*
+    we can define a functioon in php through the following syntax :)
+
+    function functionName(varType arg1, varType arg2): returnType{
+        // returnType and varType is not compulsary.
+        return output; 
+    }
+*/
+
     function playSong(){
         echo "Playing the song through playSong().";
     }
 
     playSong();
+
+
 ?>
